b5d03af0fc0e0d6481240aff322321e6d6907401
krunalvegda02 <Krunal.vegda02@gmail.com>
2025-04-19 02:05:01 +0530

    completion of UI of AUTH

--- product-billing-system/src/App.jsx
+++ product-billing-system/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
 import ForgotPassword from "./pages/auth/ForgotPassword";
+import ChangePassword from "./pages/auth/ChangePassword";
 
 function App() {
   return <ForgotPassword />
